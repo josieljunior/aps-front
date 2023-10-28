120d2c0e957e792232f4c7698cce01e1ba03095c
Jhonathadaniel <jhonathask8@hotmail.com>
2023-10-28 11:10:10 -0300

    Update point coleta

--- src/components/InfoModal.js
+++ src/components/InfoModal.js
@@ -1,16 +1,36 @@
-import React from 'react';
-import { Modal, Button } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Modal, Button, Nav, Tab } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function InfoModal({ isOpen, onRequestClose, point }) {
+  const [activeTab, setActiveTab] = useState('info');
+
   return (
     <Modal show={isOpen} onHide={onRequestClose} centered>
       <Modal.Header closeButton>
         <Modal.Title>{point && point.name}</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <p>Endereço: {point && point.address}</p>
-        <p>Horário de funcionamento: {point && point.hours}</p>
+        <Tab.Container activeKey={activeTab} onSelect={(tab) => setActiveTab(tab)}>
+          <Nav variant="tabs">
+            <Nav.Item>
+              <Nav.Link eventKey="info">Informações</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="location">Localização</Nav.Link>
+            </Nav.Item>
+          </Nav>
+          <Tab.Content>
+            <Tab.Pane eventKey="info">
+              <p>Endereço: {point && point.address}</p>
+              <p>Horário de funcionamento: {point && point.hours}</p>
+            </Tab.Pane>
+            <Tab.Pane eventKey="location">
+              <p>Latitude: {point && point.latitude}</p>
+              <p>Longitude: {point && point.longitude}</p>
+            </Tab.Pane>
+          </Tab.Content>
+        </Tab.Container>
       </Modal.Body>
       <Modal.Footer>
         <Button variant="secondary" onClick={onRequestClose}>
